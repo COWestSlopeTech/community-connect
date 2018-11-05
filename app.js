a8c092ef7c1f3a612df480b40b9fb658f99fd4c1
jmapping <j.mapping@gmail.com>
2018-11-05 11:29:41 -0700

    random

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
+const d3 = require("d3");
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
